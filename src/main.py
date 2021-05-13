6731d9919104cf4ef9e82d4811fc080bab36ba11
Cyprien Ricque <cyprien.ricque@epitech.eu>
2021-05-13 17:11:08 +0200

    create first POW blockchain with flask server

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 from argparse import ArgumentParser
-
-import flask
-from flask import Flask, request
-
-from blockchain import Blockchain
+from server import app
 
 if __name__ == '__main__':
-    bc = Blockchain()
 
-# Docker ?
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--port', default=5000, type=int, help='port to listen on')
+    args = parser.parse_args()
+    port = args.port
+
+    app.run(host='0.0.0.0', port=port)
